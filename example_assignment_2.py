ca42f9915a4af6428b6eb76ed9f8065f2ee1f456
Antoni Kowalczuk <antonikowalczuk@gmail.com>
2024-05-08 12:32:27 +0000

    add example for task 2

---
+++ example_assignment_2.py
@@ -0,0 +1,20 @@
+import requests
+import torch
+import torch.nn as nn
+
+#### SUBMISSION ####
+
+# Create a dummy model
+model = nn.Sequential(nn.Flatten(), nn.Linear(32*32*3, 1024))
+
+torch.onnx.export(
+    model,
+    torch.randn(1, 3, 32, 32),
+    "out/models/dummy_submission.onnx",
+    export_params=True,
+    input_names=["x"],
+)
+
+# Send the model to the server
+response = requests.post("http://http://35.184.239.3:9090/stealing", files={"file": open("out/models/dummy_submission.onnx", "rb")}, headers={"token": "TOKEN", "seed": "SEED"})
+print(response.json())
