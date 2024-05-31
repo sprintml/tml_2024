781adc19f4dccec4dc9a741960c40407cacea0f2
adam-dziedzic <adam.cajf@gmail.com>
2024-05-31 22:25:16 +0200

    debug

--- example_assignment_2.py
+++ example_assignment_2.py
@@ -1,17 +1,59 @@
 import requests
 import torch
 import torch.nn as nn
-# Do install: conda install onnx
+# Do install: 
+# conda install onnx
+# conda install onnxruntime
 import onnxruntime as ort
 import numpy as np
 import json
 import io
+import sys
 import base64
+from torch.utils.data import Dataset
+from typing import Tuple
+import pickle
+import os
+
+cwd = os.getcwd()
+print('cwd: ', cwd)
+
+class TaskDataset(Dataset):
+    def __init__(self, transform=None):
+
+        self.ids = []
+        self.imgs = []
+        self.labels = []
+
+        self.transform = transform
+
+    def __getitem__(self, index) -> Tuple[int, torch.Tensor, int]:
+        id_ = self.ids[index]
+        img = self.imgs[index]
+        if not self.transform is None:
+            img = self.transform(img)
+        label = self.labels[index]
+        return id_, img, label
+
+    def __len__(self):
+        return len(self.ids)
 
 ### REQUESTING NEW API ###
+TOKEN = "1" # to be changed according to your token (given to you for the assignments)
 
-response = requests.get("http://34.71.138.79:9090" + "/stealing_launch", headers={"token": "TOKEN"})
-print(response.json())  # {"seed": "SEED", "port": PORT}
+response = requests.get("http://34.71.138.79:9090" + "/stealing_launch", headers={"token": TOKEN})
+answer = response.json()
+
+print(answer)  # {"seed": "SEED", "port": PORT}
+if 'detail' in answer:
+    sys.exit(1)
+
+# save the values
+SEED = str(answer['seed'])
+PORT = str(answer['port'])
+
+# SEED = "1868949"
+# PORT = "9002"
 
 ### QUERYING THE API ###
 
@@ -27,7 +69,7 @@ def model_stealing(images, port):
         image_data.append(img_base64)
     
     payload = json.dumps(image_data)
-    response = requests.get(url, files={"file": payload}, headers={"token": "TOKEN"})
+    response = requests.get(url, files={"file": payload}, headers={"token": TOKEN})
     if response.status_code == 200:
         representation = response.json()["representations"]
         return representation
@@ -37,8 +79,18 @@ def model_stealing(images, port):
         )
 
 dataset = torch.load("ModelStealingPub.pt")
-out = model_stealing([dataset.imgs[idx] for idx in np.random.permutation(1000)])
-print(out.shape)
+out = model_stealing([dataset.imgs[idx] for idx in np.random.permutation(1000)], port=PORT)
+
+# Store the output in a file.
+# Be careful to store all the outputs from the API since the number of queries is limited.
+with open('out.pickle', 'wb') as handle:
+    pickle.dump(out, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+# Restore the output from the file.
+with open('out.pickle', 'rb') as handle:
+    out = pickle.load(handle)
+
+print(len(out))
 
 #### SUBMISSION ####
 
@@ -46,8 +98,6 @@ print(out.shape)
 model = nn.Sequential(nn.Flatten(), nn.Linear(32*32*3, 1024))
 
 path = 'dummy_submission.onnx'
-TOKEN = "1" # to be changed according to your token (given to you for the assignments)
-SEED = "1" # to be changed according to your seed (from the API endpoint that you run locally)
 
 torch.onnx.export(
     model,
